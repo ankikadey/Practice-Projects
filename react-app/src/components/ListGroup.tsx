65b1dbd01ad2061e1b17a1d1574928103d7c33e8
Ankika Dey <ankikadey@outlook.com>
2024-01-03 20:31:16 +0530

    Update ListGroup.tsx

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,24 +1,18 @@
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+  const getMessage = () => { 
+    return items.length == 0 ? <p>No item found</p> : null;
+  }
 
-    return (
-        <>
-        <h1>List</h1>
-        <ul className="list-group">
-            {items.map ((item) => (<li>{item}</li>))}
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+  return (
+    <>
+      <h1>List</h1>
+      {} 
+      <ul className="list-group">
+        {getMessage()}
+      </ul>
     </>
   );
 }
