ed999fd74da336f4d30e24aac5999d0deedcfd55
Ankika Dey <ankikadey@outlook.com>
2024-01-03 20:19:28 +0530

    Update ListGroup.tsx

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,8 +1,18 @@
 function ListGroup() {
-  return (
-    <>
+
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    return (
+        <>
         <h1>List</h1>
         <ul className="list-group">
+            {items.map ((item) => (<li>{item}</li>))}
           <li className="list-group-item">An item</li>
           <li className="list-group-item">A second item</li>
           <li className="list-group-item">A third item</li>
